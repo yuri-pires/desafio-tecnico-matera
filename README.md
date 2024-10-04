3046381789da153a537416cc8a7ccf489c99d612
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-10-03 21:56:16 -0300

    feat(project): estrutura inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Desafio técnico Matera
+
+Este projeto tem como objetivo automatizar testes backend de API Rest na API [Cat Facts](https://catfact.ninja) 🐈
+
+### Pré-requisitos
+
+- **[Java 11 ou superior](https://www.oracle.com/java/technologies/downloads/)**: O projeto é desenvolvido em Java, então você precisa ter o Java JDK instalado.
+- **[Maven](https://maven.apache.org/install.html)**: Usado para gerenciar dependências e construir o projeto.
---
+++ src/test/java/org/matera/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package org.matera;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("org.matera")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.matera")
+public class RunCucumberTest {
+}
