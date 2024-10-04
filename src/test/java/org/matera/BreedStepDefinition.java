f0e0090326a7e83807e85d241e7f0ed2ebf76103
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-10-04 12:15:55 -0300

    feat(tests): testes de segurança e exceção de erros no endpoint GET /breeds

--- src/test/java/org/matera/BreedStepDefinition.java
+++ src/test/java/org/matera/BreedStepDefinition.java
@@ -9,9 +9,11 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BreedStepDefinition {
   private Response response;
+  private boolean isConnectionClosed;
 
   @Given("I have access to the Cat Facts API")
   public void iHaveAccessToTheCatFactAPi() {
@@ -20,13 +22,51 @@ public class BreedStepDefinition {
 
   @When("I send a GET request to the breeds endpoint with a limit of {int} for the breeds list")
   public void iSendAGetRequestToTheBreedsEndpointWithLimitParameter(Integer limitQueryValue) {
-
     response =
       given()
         .param("limit", limitQueryValue)
         .header("accept", "application/json")
       .when()
         .get("/breeds");
+  }
+
+  /**
+   * Teste de segurança que valida o comportamento do servidor ao receber um Header "Host" inválido.
+   * Usa um bloco {@code try-catch} para capturar exceções de conexão, já que o servidor pode fechar a conexão,
+   * o que resultaria em uma falha no Java. O objetivo é garantir que o servidor responda corretamente fechando a
+   * conexão, protegendo contra ataques de manipulação do cabeçalho "Host".
+   */
+  @When("I send a GET request to the breeds endpoint with invalid Host header")
+  public void iSendAGetRequestToTheBreedsEndpointWithInvalidHostHeader() {
+    try {
+      response =
+        given()
+          .param("limit", 1)
+          .header("accept", "application/json")
+          .header("Host", "api.fake.com")
+        .when()
+          .get("/breeds");
+
+      isConnectionClosed = false;
+    } catch (Exception e) {
+      System.out.println("org.apache.http.NoHttpResponseException handle this request -> " + e);
+      isConnectionClosed = true;
+    }
+  }
+
+  @When("I send a POST request instead of a GET request to the breeds endpoint")
+  public void iSendAPOSTRequestInsteadOfAGETRequestToTheBreedsEndpoint() {
+    response =
+      given()
+        .param("limit", 1)
+        .header("accept", "application/json")
+      .when()
+        .post("/breeds");
+  }
+
+  @And("I should receive a connection close from the server")
+  public void iShouldReceiveAConnectionCloseFromTheServer() {
+    assertTrue(isConnectionClosed);
   }
 
   @Then("I should receive a status code of {int}")
@@ -54,5 +94,12 @@ public class BreedStepDefinition {
           hasKey("pattern")
         )
       ));
+  }
+
+  @And("I should receive a message with the reason Not Found")
+  public void iShouldReceiveAMessageWithTheReasonNotFound(){
+    response.then()
+      .body("message", equalTo("Not Found"))
+      .body("code", equalTo(404));
   }
 }
--- src/test/resources/org/matera/breeds.feature
+++ src/test/resources/org/matera/breeds.feature
@@ -13,3 +13,14 @@ Feature: GET /breeds endpoint
     Then I should receive a status code of 200
     And I should have a list of breeds containing exactly 6 items
     And each breed object should contain the following fields - breed, country, origin, coat, pattern
+
+  Scenario: Handling invalid method request to the breeds endpoint
+    Given I have access to the Cat Facts API
+    When I send a POST request instead of a GET request to the breeds endpoint
+    Then I should receive a status code of 404
+    And I should receive a message with the reason Not Found
+    
+  Scenario: Handling invalid Host header in GET request to breeds endpoint
+    Given I have access to the Cat Facts API
+    When I send a GET request to the breeds endpoint with invalid Host header
+    And I should receive a connection close from the server
