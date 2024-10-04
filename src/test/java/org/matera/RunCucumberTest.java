91db448422aae82e17692c8f7a8b511b77ff00e4
Yuri Espinosa Pires <110349970+yuri-pires@users.noreply.github.com>
2024-10-04 00:33:37 -0300

    fix(cucumber): ajuste na anotação que gera o report .html

--- src/test/java/org/matera/RunCucumberTest.java
+++ src/test/java/org/matera/RunCucumberTest.java
@@ -11,8 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("org.matera")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-report.html")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "html:target/cucumber-reports/cucumber.html")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.matera")
 public class RunCucumberTest {
 }
